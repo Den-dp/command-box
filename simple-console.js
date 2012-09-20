ee2251a97bb5462d1249217beba70ceadc65821e
Den_dp <Denis.Bendrikov@gmail.com>
2012-09-20 16:50:12 +0300

    provide basic implementation for Console class with basic usage

--- simple-console.js
+++ simple-console.js
@@ -4,3 +4,16 @@
  * Date: 20.09.12
  * Time: 16:10
  */
+
+(function( scoupe ){
+	scoupe.Console = function Console( config, commandsArray ){
+		var input = document.querySelector( config.selector );
+		this.commands = commandsArray;
+
+		input.addEventListener( 'input', function(){
+			console.log( 'm-m-m-message' );
+		});
+		
+	}
+
+})( this );
