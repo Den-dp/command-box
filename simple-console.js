37d7786078b74556c52e3bf064ccca4390adaaa1
Den_dp <Denis.Bendrikov@gmail.com>
2012-09-21 21:08:20 +0300

    provide fuzzy search for stored commands: add grep function with jsdoc add some using grep function for simple fuzy search

--- simple-console.js
+++ simple-console.js
@@ -8,6 +8,26 @@
 (function( scoupe ){
 
 	/**
+	 * Finds the elements of an array which satisfy a filter function.
+	 * The original array is not affected
+	 *
+	 * @param objArray
+	 * @param filter (elementOfArray,indexInArray) is a function that provides criteries for
+	 *        grepping passed objects
+	 * @return {Array}
+	 */
+	function grep( objArray, filter ){
+		var greppedItems = [];
+		for ( var i in objArray ) {
+			var temp = filter( i, objArray[i] );
+			if( !!temp ){
+				greppedItems.push( temp );
+			}
+		}
+		return greppedItems;
+	}
+
+	/**
 	 * Console constructor-function
 	 *
 	 * @param config is object with basic configs
@@ -17,9 +37,15 @@
 	scoupe.Console = function Console( config, commandsArray ){
 		var input = document.querySelector( config.selector );
 		this.commands = commandsArray;
+		var self = this;
 
 		input.addEventListener( 'input', function(){
-			console.log( 'm-m-m-message' );
+			var results = grep( self.commands, function( _, obj ){
+				if( obj.name.toLowerCase().indexOf( input.value.toLowerCase() ) != -1 ){
+					return obj;
+				}
+			});
+			console.log( results );
 		});
 		
 	}
